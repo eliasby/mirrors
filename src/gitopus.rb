52fc8b7646bc080fefac6b08c6f3f1fcd1143cfe
Elias Bouassaba <ebouassaba@gmail.com>
2017-03-07 00:19:27 +0000

    Initial commit

---
+++ src/gitopus.rb
@@ -0,0 +1,21 @@
+require 'json'
+
+class Program
+    def self.run
+        file = File.read('remotes.json')
+        json = JSON.parse(file)
+        
+        primary = json['primary']
+        mirrors = json['mirrors']
+
+        system 'git remote rm origin'
+        system "git remote add origin #{primary}"
+        system "git remote set-url --add --push origin #{primary}"
+
+        mirrors.each do |mirror|
+            system "git remote set-url --add --push origin #{mirror}"
+        end
+
+        system 'git remote -v'
+    end
+end
