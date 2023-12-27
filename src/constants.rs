5d9a6d074faf0dc274055cba0a4ddad6d9deba2e
Elias Bouassaba <ebouassaba@gmail.com>
2023-12-27 06:04:14 +0100

    fix: update DOCS_URL

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,2 @@
 pub const CONFIG_FILENAME: &str = "mirrors.json";
-pub const DOCS_URL: &str = "https://github.com/eli-ba/mirrors/blob/main/README.md";
+pub const DOCS_URL: &str = "https://github.com/eliasby/mirrors/blob/main/README.md";
